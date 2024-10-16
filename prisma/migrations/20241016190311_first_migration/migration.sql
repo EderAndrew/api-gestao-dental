81c803c67f8f9c580a00a887958c5c0e6b0986d0
Eder Andrew <ederandrew0028@gmail.com>
2024-10-16 16:04:42 -0300

    first migration

---
+++ prisma/migrations/20241016190311_first_migration/migration.sql
@@ -0,0 +1,213 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'BACKOFFICE', 'PATIENT');
+
+-- CreateEnum
+CREATE TYPE "PaymentType" AS ENUM ('CREDIT', 'DEBIT', 'MONEY');
+
+-- CreateTable
+CREATE TABLE "addresses" (
+    "id" SERIAL NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "complement" TEXT,
+    "neighborhood" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "userId" INTEGER,
+    "officeId" TEXT,
+    "patientId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "addresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "anamneses" (
+    "id" SERIAL NOT NULL,
+    "reason" TEXT NOT NULL,
+    "discomfort_mouth" TEXT NOT NULL,
+    "high_pressure" TEXT NOT NULL,
+    "control_pressure" TEXT NOT NULL,
+    "oral_hygiene" TEXT NOT NULL,
+    "grind_teeth" TEXT NOT NULL,
+    "allergy" TEXT NOT NULL,
+    "what_allergy" TEXT NOT NULL,
+    "drink_smoke" TEXT NOT NULL,
+    "frequency" TEXT NOT NULL,
+    "bleeding" TEXT NOT NULL,
+    "when_bleeding" TEXT NOT NULL,
+    "sensitivity" TEXT NOT NULL,
+    "prothesis" TEXT NOT NULL,
+    "prothesis_type" TEXT NOT NULL,
+    "pregnant_breastfeeding" TEXT,
+    "pregnant_time" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "patientId" INTEGER NOT NULL,
+
+    CONSTRAINT "anamneses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "budgets" (
+    "id" SERIAL NOT NULL,
+    "treatment_type" TEXT NOT NULL,
+    "treatment_date" TEXT NOT NULL,
+    "procedure_type" TEXT NOT NULL,
+    "agreement" TEXT NOT NULL,
+    "total" INTEGER NOT NULL,
+    "entry" INTEGER,
+    "pay_installments" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "patientId" INTEGER NOT NULL,
+
+    CONSTRAINT "budgets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "offices" (
+    "id" SERIAL NOT NULL,
+    "identity" TEXT,
+    "corporate" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "cnpj" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "offices_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" SERIAL NOT NULL,
+    "payment" TEXT NOT NULL,
+    "split_payment" TEXT,
+    "paymentType" "PaymentType" NOT NULL DEFAULT 'DEBIT',
+    "payday" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3),
+    "userId" INTEGER NOT NULL,
+    "officeIdentity" TEXT NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" SERIAL NOT NULL,
+    "identity" TEXT,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "cpf" TEXT NOT NULL,
+    "rg" TEXT NOT NULL,
+    "birth_date" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "career" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "responsible_name" TEXT,
+    "responsible_birth_date" TEXT,
+    "contact_name" TEXT,
+    "contact_tel" TEXT,
+    "agreement" TEXT NOT NULL,
+    "agreement_card" TEXT NOT NULL,
+    "holder_name" TEXT,
+    "cpf_holder" TEXT,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "role" "Role" NOT NULL DEFAULT 'PATIENT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "officeId" TEXT NOT NULL,
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" SERIAL NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "timestamps" BOOLEAN NOT NULL DEFAULT true,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teeths" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "position" TEXT NOT NULL,
+    "faces" TEXT NOT NULL,
+    "option" TEXT NOT NULL,
+    "value" INTEGER NOT NULL,
+    "discount" INTEGER NOT NULL,
+    "budgetId" INTEGER NOT NULL,
+
+    CONSTRAINT "teeths_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "tel" TEXT,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "officeId" TEXT,
+    "firstAccess" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "offices_identity_key" ON "offices"("identity");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "offices_cnpj_key" ON "offices"("cnpj");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_identity_key" ON "patients"("identity");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_officeId_fkey" FOREIGN KEY ("officeId") REFERENCES "offices"("identity") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("identity") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "anamneses" ADD CONSTRAINT "anamneses_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "budgets" ADD CONSTRAINT "budgets_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_officeIdentity_fkey" FOREIGN KEY ("officeIdentity") REFERENCES "offices"("identity") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients" ADD CONSTRAINT "patients_officeId_fkey" FOREIGN KEY ("officeId") REFERENCES "offices"("identity") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teeths" ADD CONSTRAINT "teeths_budgetId_fkey" FOREIGN KEY ("budgetId") REFERENCES "budgets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_officeId_fkey" FOREIGN KEY ("officeId") REFERENCES "offices"("identity") ON DELETE SET NULL ON UPDATE CASCADE;
